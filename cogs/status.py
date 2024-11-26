e08bc36ddcadd0bc97d9c31be4ffa185e00671de
techfish-11 <msuke1018@icloud.com>
2024-11-26 18:18:36 +0900

    status fix

--- cogs/status.py
+++ cogs/status.py
@@ -25,7 +25,7 @@ class Status(commands.Cog):
         embed.add_field(name="Discord APIレイテンシ", value=f"{discord_latency}ms", inline=False)
         embed.add_field(name="ネットワークルーターレイテンシ", value=f"{router_latency}ms", inline=False)
         embed.add_field(name="ステータス詳細", value="[こちらをご覧ください](https://status.sakana11.org)", inline=False)
-        embed.add_field(name="Deploy RunnerでLemonBotは動作しています。", inline=False)
+        embed.add_field(name="Deploy RunnerでLemonBotは動作しています。", value="正常に稼働中です。", inline=False)
 
         await interaction.response.send_message(embed=embed)
 
